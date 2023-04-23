832ee74566a5eb01136a4090c0077defd25acad1
t770113 <wallace.oliveira@santander.com.br>
2023-04-23 13:05:27 -0300

    feat: Curtir e excluir foto

--- src/app/components/animais/detalhe-animal/detalhe-animal.component.ts
+++ src/app/components/animais/detalhe-animal/detalhe-animal.component.ts
@@ -2,7 +2,7 @@ import { AnimaisService } from './../services/animais.service';
 import { Animal } from './../models/animais';
 import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-detalhe-animal',
@@ -17,11 +17,34 @@ export class DetalheAnimalComponent implements OnInit {
   constructor(
     private animaisService: AnimaisService,
     private activatedRoute: ActivatedRoute,
+    private router: Router,
   ) { }
 
   ngOnInit(): void {
+    this.buscarPorId();
+  }
+
+  buscarPorId() {
     this.animalId = this.activatedRoute.snapshot.params?.['animalId'];
     this.animal$ = this.animaisService.buscarPorId(this.animalId);
   }
 
+  curtir() {
+    this.animaisService.curtir(this.animalId).subscribe((curtida) => {
+      if (curtida) {
+        this.animal$ = this.animaisService.buscarPorId(this.animalId);
+      }
+    });
+  }
+
+  excluir() {
+    this.animaisService.excluiAnimal(this.animalId).subscribe(
+      () => {
+        this.router.navigate(['/animais/']);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
--- src/app/components/animais/services/animais.service.ts
+++ src/app/components/animais/services/animais.service.ts
@@ -31,7 +31,7 @@ export class AnimaisService {
 
   curtir(id: number): Observable<boolean> {
     return this.http
-      .post(`${API}/photos/${id}/likes`, {}, { observe: 'response' })
+      .post(`${API}/photos/${id}/like`, {}, { observe: 'response' })
       .pipe(
         mapTo(true),
         catchError((error) => {
