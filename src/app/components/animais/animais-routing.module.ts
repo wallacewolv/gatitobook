38d81579ac5f126c256acda5a1e004df62b7f5d6
t770113 <wallace.oliveira@santander.com.br>
2023-04-23 12:13:21 -0300

    feat: Componente de detalhe

--- src/app/components/animais/animais-routing.module.ts
+++ src/app/components/animais/animais-routing.module.ts
@@ -1,3 +1,4 @@
+import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
 import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -6,6 +7,10 @@ const routes: Routes = [
   {
     path: '',
     component: ListaAnimaisComponent
+  },
+  {
+    path: ':animalId',
+    component: DetalheAnimalComponent
   }
 ];
 
