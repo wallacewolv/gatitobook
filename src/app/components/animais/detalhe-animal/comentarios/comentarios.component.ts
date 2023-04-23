9ba14217141add2787a9cf1f90eee409032e6ba1
t770113 <wallace.oliveira@santander.com.br>
2023-04-23 15:23:42 -0300

    feat: Componente

--- src/app/components/animais/detalhe-animal/comentarios/comentarios.component.ts
+++ src/app/components/animais/detalhe-animal/comentarios/comentarios.component.ts
@@ -1,6 +1,9 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable, switchMap, tap } from 'rxjs';
+
 import { Comentarios } from './models/comentarios';
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { ComentariosService } from './services/comentarios.service';
 
 @Component({
   selector: 'app-comentarios',
@@ -9,11 +12,42 @@ import { Observable } from 'rxjs';
 })
 export class ComentariosComponent implements OnInit {
 
+  @Input() id!: number;
   comentarios$!: Observable<Comentarios>;
+  comentarioform!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private comentariosService: ComentariosService,
+    private formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit(): void {
+    this.buscarComentario();
+    this.incicializarComentarioForm();
   }
 
+  buscarComentario() {
+    this.comentarios$ = this.comentariosService.buscaComentario(this.id);
+  }
+
+  incicializarComentarioForm() {
+    this.comentarioform = this.formBuilder.group({
+      comentario: ['', Validators.maxLength(300)],
+    })
+  }
+
+  gravar() {
+    const comentario = this.comentarioform.get('comentario')?.value ?? '';
+    this.comentarios$ = this.comentariosService.incluiComentario(
+      this.id, comentario)
+      .pipe(
+        switchMap(() => this.comentariosService.buscaComentario(this.id)),
+        tap(() => {
+          // Usando o tap para efeitos colatarais,
+          // não afetam o fluxo mas precisam acontecer, Ex: resetar o form
+          this.comentarioform.reset();
+          alert("Salvo Comentário");
+        })
+      );
+  }
 }
