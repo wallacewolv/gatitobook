20a0c7cdb432080ad034bc75f9506097ed0744e4
wallace <wallace_wolv@hotmail.com>
2023-05-04 22:24:17 -0300

    feat: Serviço de upload

--- src/app/components/animais/services/animais.service.ts
+++ src/app/components/animais/services/animais.service.ts
@@ -9,13 +9,10 @@ const API = environment.apiURL;
 const NOT_MODIFIED = '304';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AnimaisService {
-
-  constructor(
-    private http: HttpClient,
-  ) { }
+  constructor(private http: HttpClient) {}
 
   listaDoUsuario(nomeDoUsuario: string): Observable<Animais> {
     return this.http.get<Animais>(`${API}/${nomeDoUsuario}/photos`);
@@ -38,5 +35,17 @@ export class AnimaisService {
           return error.status === NOT_MODIFIED ? of(false) : throwError(error);
         })
       );
+  }
+
+  upload(descricao: string, permiteComentario: boolean, arquivo: File) {
+    const formData = new FormData();
+    formData.append('description', descricao);
+    formData.append('allowComments', permiteComentario ? 'true' : 'false');
+    formData.append('imageFile', arquivo);
+
+    return this.http.post(`${API}/photos/upload`, formData, {
+      observe: 'events',
+      reportProgress: true,
+    });
   }
 }
